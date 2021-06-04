04311fd19fb8db0f785113ee63b0c55ae7e64fb2
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-04 22:16:49 +0300

    for Currency Converter / ad359e8

---
+++ assets/selectFromData.php
@@ -0,0 +1,10 @@
+<?php
+//The currently supported base currencies
+
+$fromCurrencies = array(
+    "USD" => "United States Dollars - USD" ,
+    "EUR" => "Euro - EUR" ,
+    "GBP" => "United Kingdom Pounds - GBP" ,
+    "CHF" => "Switzerland Francs - CHF"
+);
+?>
