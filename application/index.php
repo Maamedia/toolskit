a854f4cdabc3588f8ebf3ca83e84169a5d3f9968
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-04 22:04:54 +0300

    Toolskit Main page

---
+++ application/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-language" content="en">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=yes, width=device-width, shrink-to-fit=no">
+    <meta http-equiv="imagetoolbar" content="no"/>
+    <title>Toolskit | Maamedia Tools</title>
+    <link rel="stylesheet" href="https://maamedia.org/toolskit/css/toolskit.bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="https://maamedia.org/toolskit/css/toolskit.css">
+    <script src="https://maamedia.org/toolskit/js/toolskit.search.js"></script>
+      </head>
+  <body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Maamedia.org</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/toolskit">All Tools</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="nb-seealso" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">See also</a>
+            <div class="dropdown-menu" aria-labelledby="nb-seealso">
+              <a class="dropdown-item" href="https://maamedia.org/server">Maamedia Servers Usage</a>
+              <a class="dropdown-item" href="https://meta.maamedia.org/wiki/Certified_Maamedia_tools">Tools Certificates</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="https://meta.maamedia.org/wiki/Special:MyLanguage/Maamedia_Tools">About tools</a>
+              <a class="dropdown-item" href="https://maamedia.org/coding">Coding in Maamedia</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="https://maamedia.org/toolskit/jobhistory">Jobs History (Still Development.)</a>
+              <a class="dropdown-item" href="https://maamedia.org/contact/">Contact to Maamedia</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="https://meta.maamedia.org/wiki/Special:CreateAccount">Create wiki-account</a>
+              <a class="dropdown-item" href="https://meta.maamedia.org/wiki/Maamedia_wikis">Maamedia wikis</a>
+              <a class="dropdown-item" href="https://meta.maamedia.org/wiki/Server_admin_log">Server Admin Log</a>
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto justify-content-end">
+          <li class="nav-item ">
+            <a class="nav-link" href="https://github.com/maamedia/toolskit">Source code</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="https://maamedia.org/toolskit/help">Help</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div id="content" class="container-fluid bg-white">
+            <ol class="breadcrumb">
+  <li class="breadcrumb-item active">Tools</li>
+</ol>
+
+<h1>All Tools</h1>
+<div id="status" class="alert alert-info" role="alert">
+  <button type="button" class="close all-toolinfo" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <p class="m-0 p-0"></p>
+</div>
+<form id="search">
+  <div class="form-group form-row">
+    <label for="q" class="col-sm-2 col-form-label">Search tools:</label>
+    <div class="col-sm-8">
+      <input type="text" class="form-control" id="q">
+    </div>
+    <div class="col-sm-2">
+      <button type="button" class="btn btn-info all-toolinfo">All</button>
+    </div>
+  </div>
+</form>
+<div class="card-deck"><div class="card toolinfo" data-tool="barcodegenerator">
+    <div class="card-body">
+      <h4 class="card-title">
+                Barcode Generator
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="barcodegenerator.php">Barcode Generator</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="codeeditor">
+    <div class="card-body">
+      <h4 class="card-title">
+                Live Code Editor
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="codeeditor.php">Code Editor</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="currencyconverter">
+    <div class="card-body">
+      <h4 class="card-title">
+                Currency Converter
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="currencyconverter.php">Currency Converter</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="md5">
+    <div class="card-body">
+      <h4 class="card-title">
+                MD5 Hash Generator
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="md5.php">MD5 Hash Generator</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="privcacypolicygenerator">
+    <div class="card-body">
+      <h4 class="card-title">
+                Privacy Policy Generator
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="privacypolicygenerator.php">Privacy Policy Generator</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div>
+ </div>
+           </div>
+    <footer role="contentinfo">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <ul class="nav justify-content-center">
+              <li class="nav-item"><a class="nav-link" href="https://meta.maamedia.org/wiki/Special:MyLanguage/Privacy_policy">Maamedia Privacy Policy</a></li>
+              <li class="nav-item"><a class="nav-link" href="https://meta.maamedia.org/wiki/Special:MyLanguage/Terms_of_Use">Terms of Use</a></li>
+              <li class="nav-item"><a class="nav-link" href="https://meta.maamedia.org/wiki/Maamedia_developers">Maamedia Developers</a></li>
+              <li class="nav-item"><a class="nav-link" href="https://phabricator.maamedia.org/maniphest/task/edit/form/3/">Report a bug</a></li>
+              <li class="nav-item"><a class="nav-link" href="https://github.com/Maamedia/toolskit/releases">Toolskit Productions</a></li>
+              <li class="nav-item"><a class="nav-link" href="https://meta.maamedia.org/wiki/Certified_Maamedia_tools">Certificates</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col col-md-2">
+            <a href="/">
+              <img src="https://commons.maamedia.org/images/4/40/Powered_by_MaamediaTools.png" alt="Powered by Maamedia Tools" style="width:120px; height:120px;">
+            </a>
+          </div>
+          <div class="col col-md-10 text-right">
+            <p>See source code in <a href="https://github.com/Maamedia/toolskit">GitHub</a>.
+          </div>
+        </div>
+      </div>
+  </footer>
+<script src="https://maamedia.org/toolskit/js/jquery.slim.toolskit.js"></script>
+<script src="https://maamedia.org/toolskit/js/toolskit.popper.js"></script>
+<script src="https://maamedia.org/toolskit/js/bootstrap.toolskit.js"></script>
+
+  </body>
+</html>
+
