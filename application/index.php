ac69cde15eb34c972f95313442c58c8ae00ffc33
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-15 18:35:42 +0300

    new file - UPDATE

--- application/index.php
+++ application/index.php
@@ -56,76 +56,9 @@
 </ol>
 
 <h1>All Tools</h1>
-<div id="status" class="alert alert-info" role="alert">
-  <button type="button" class="close all-toolinfo" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-  <p class="m-0 p-0"></p>
-</div>
-<form id="search">
-  <div class="form-group form-row">
-    <label for="q" class="col-sm-2 col-form-label">Search tools:</label>
-    <div class="col-sm-8">
-      <input type="text" class="form-control" id="q">
-    </div>
-    <div class="col-sm-2">
-      <button type="button" class="btn btn-info all-toolinfo">All</button>
-    </div>
-  </div>
-</form>
-<div class="card-deck"><div class="card toolinfo" data-tool="barcodegenerator">
-    <div class="card-body">
-      <h4 class="card-title">
-                Barcode Generator
-              </h4>
-      <h6 class="card-subtitle mb-2 text-muted">
-        Tool: <a href="barcodegenerator.php">Barcode Generator</a>
-      </h6>
-      <div class="card-text"></div>
-    </div>
-  </div><div class="card toolinfo" data-tool="codeeditor">
-    <div class="card-body">
-      <h4 class="card-title">
-                Live Code Editor
-              </h4>
-      <h6 class="card-subtitle mb-2 text-muted">
-        Tool: <a href="codeeditor.php">Code Editor</a>
-      </h6>
-      <div class="card-text"></div>
-    </div>
-  </div><div class="card toolinfo" data-tool="currencyconverter">
-    <div class="card-body">
-      <h4 class="card-title">
-                Currency Converter
-              </h4>
-      <h6 class="card-subtitle mb-2 text-muted">
-        Tool: <a href="currencyconverter.php">Currency Converter</a>
-      </h6>
-      <div class="card-text"></div>
-    </div>
-  </div><div class="card toolinfo" data-tool="md5">
-    <div class="card-body">
-      <h4 class="card-title">
-                MD5 Hash Generator
-              </h4>
-      <h6 class="card-subtitle mb-2 text-muted">
-        Tool: <a href="md5.php">MD5 Hash Generator</a>
-      </h6>
-      <div class="card-text"></div>
-    </div>
-  </div><div class="card toolinfo" data-tool="privcacypolicygenerator">
-    <div class="card-body">
-      <h4 class="card-title">
-                Privacy Policy Generator
-              </h4>
-      <h6 class="card-subtitle mb-2 text-muted">
-        Tool: <a href="privacypolicygenerator.php">Privacy Policy Generator</a>
-      </h6>
-      <div class="card-text"></div>
-    </div>
-  </div>
- </div>
-           </div>
+<?php 
+include "/var/www/html/main/toolskit/toolslist.php"
+?>
     <footer role="contentinfo">
       <div class="container">
         <div class="row">
