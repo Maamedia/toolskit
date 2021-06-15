f055d0d02390bcd3391b6f760fdc080e8c486f15
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-15 18:41:55 +0300

    list of Maamedia tools

---
+++ toolslist.php
@@ -0,0 +1,80 @@
+<div id="status" class="alert alert-info" role="alert">
+  <button type="button" class="close all-toolinfo" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <p class="m-0 p-0"></p>
+</div>
+<form id="search">
+  <div class="form-group form-row">
+    <label for="q" class="col-sm-2 col-form-label">Search tools:</label>
+    <div class="col-sm-8">
+      <input type="text" class="form-control" id="q">
+    </div>
+    <div class="col-sm-2">
+      <button type="button" class="btn btn-info all-toolinfo">All</button>
+    </div>
+  </div>
+</form>
+<div class="card-deck"><div class="card toolinfo" data-tool="barcodegenerator">
+    <div class="card-body">
+      <h4 class="card-title">
+                Proofreader
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="https://maamedia.org/toolskit/proofread.php">Proofreader</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+    </div><div class="card toolinfo" data-tool="barcodegenerator">
+        <div class="card-body">
+      <h4 class="card-title">
+                Barcode Generator
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="https://maamedia.org/toolsit/barcodegenerator.php">Barcode Generator</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="codeeditor">
+    <div class="card-body">
+      <h4 class="card-title">
+                Live Code Editor
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="https://maamedia.org/toolsit/codeeditor.php">Code Editor</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="currencyconverter">
+    <div class="card-body">
+      <h4 class="card-title">
+                Currency Converter
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="https://maamedia.org/toolsit/currencyconverter.php">Currency Converter</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="md5">
+    <div class="card-body">
+      <h4 class="card-title">
+                MD5 Hash Generator
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href="https://maamedia.org/toolsit/md5.php">MD5 Hash Generator</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div><div class="card toolinfo" data-tool="privcacypolicygenerator">
+    <div class="card-body">
+      <h4 class="card-title">
+                Privacy Policy Generator
+              </h4>
+      <h6 class="card-subtitle mb-2 text-muted">
+        Tool: <a href=https://maamedia.org/toolsit/privacypolicygenerator.php">Privacy Policy Generator</a>
+      </h6>
+      <div class="card-text"></div>
+    </div>
+  </div>
+ </div>
+           </div>
