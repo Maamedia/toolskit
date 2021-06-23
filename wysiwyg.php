2499494868936f2249f77060d05ed3b914aa1668
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-23 19:40:12 +0300

    Wysiwyg Editor = https://maamedia.org/toolskit/wysiwyg.php

---
+++ wysiwyg.php
@@ -0,0 +1,75 @@
+<title>Wysiwyg Editor | Maamedia Tools</title>
+<html lang="en"></html>
+<meta name="description" content="Wysiwyg Editor (free online) by Maamedia Tools.">
+<meta name="version" content="11.3.2 Beta WYMT">
+<link href="css/wysiwyg.css" rel="stylesheet">
+<div style="font-family:sans-serif;">
+    <div style="text-align:center; font-size:150%; color:green">Free online Wysiwyg Editor by Maamedia Tools.</div>
+</div>
+<div id="toolbar">
+                <span class="mt-formats">
+                  <select class="mt-header">
+                    <option value="1">Heading</option>
+                    <option value="2">Subheading</option>
+                    <option selected>Normal</option>
+                  </select>
+                  <select class="mt-font">
+                    <option selected>Sailec Light</option>
+                    <option value="sofia">Sofia Pro</option>
+                    <option value="slabo">Slabo 27px</option>
+                    <option value="roboto">Roboto Slab</option>
+                    <option value="inconsolata">Inconsolata</option>
+                    <option value="ubuntu">Ubuntu Mono</option>
+                  </select>
+                </span>
+                <span class="mt-formats">
+                  <button class="mt-bold"></button>
+                  <button class="mt-italic"></button>
+                  <button class="mt-underline"></button>
+                  <button class="mt-strike"></button>
+
+                      
+                  </select>
+                </span>
+                <span class="mt-formats">
+                  <button class="mt-list" value="ordered"></button>
+                  <button class="mt-list" value="bullet"></button>
+                  <select class="mt-align">
+                    <option label="left" selected></option>
+                    <option label="center" value="center"></option>
+                    <option label="right" value="right"></option>
+                    <option label="justify" value="justify"></option>
+                  </select>
+                </span>
+                <span class="mt-formats">
+                  <button class="mt-link"></button>
+                  <button class="mt-image"></button>
+                  <button class="mt-video"></button>
+                </span>
+                <span class="mt-formats">
+                  <button class="mt-formula"></button>
+                  <button class="mt-code-block"></button>
+                </span>
+                <span class="mt-formats">
+                  <button class="mt-clean"></button>
+                </span>
+                <span class="mt-formats">
+                    <button class="mt-header"><svg viewBox="0 0 18 18"> <path class="ql-fill" d="M10,4V14a1,1,0,0,1-2,0V10H3v4a1,1,0,0,1-2,0V4A1,1,0,0,1,3,4V8H8V4a1,1,0,0,1,2,0Zm6.06787,9.209H14.98975V7.59863a.54085.54085,0,0,0-.605-.60547h-.62744a1.01119,1.01119,0,0,0-.748.29688L11.645,8.56641a.5435.5435,0,0,0-.022.8584l.28613.30762a.53861.53861,0,0,0,.84717.0332l.09912-.08789a1.2137,1.2137,0,0,0,.2417-.35254h.02246s-.01123.30859-.01123.60547V13.209H12.041a.54085.54085,0,0,0-.605.60547v.43945a.54085.54085,0,0,0,.605.60547h4.02686a.54085.54085,0,0,0,.605-.60547v-.43945A.54085.54085,0,0,0,16.06787,13.209Z"></path> </svg></button>
+                    <button class="mt-blockquote"></button>
+                </span>
+</div>
+
+<div id="editor">
+</div>
+
+<script src="js/wysiwyg.js"></script>
+
+<script>
+  var editor = new Quill('#editor', {
+    modules: { toolbar: '#toolbar' },
+    theme: 'snow'
+  });
+</script>
+
+<div class="vector-body multicol-normal" id="bodyContent" style="font-family: sans-serif; font-size: calc(0.875em); line-height: 1.6; position: relative; z-index: 0;"><div class="mw-body-content mw-content-ltr" dir="ltr" id="mw-content-text" lang="fi" style="direction: ltr;"><div class="mw-parser-output"><div style="background-color: #f0d000; border-color: rgb(202, 149, 16) rgb(218, 165, 32) rgb(218, 165, 32) rgb(202, 149, 16); border-image: initial; border-radius: 10px; border-style: solid; border-width: 0.5em; float: left; margin: 0.5em; padding: 0em; text-align: center; width: 280px;"><div style="color: #202122; margin-top: -5px;"><p style="margin: 0.5em 0px;"><a class="image" href="https://g.foolcdn.com/editorial/images/478634/gold-earth.jpg" style="background: none; color: #0645ad; text-decoration-line: none;"><img alt="Puzzle globe banner, bright and close.png" data-file-height="172" data-file-width="797" decoding="async" height="183" src="https://g.foolcdn.com/editorial/images/478634/gold-earth.jpg" srcset="//https://g.foolcdn.com/editorial/images/478634/gold-earth.jpg 1.5x, //https://g.foolcdn.com/editorial/images/478634/gold-earth.jpg 2x" style="border: 0px; margin: 0px; vertical-align: middle;" width="268" /></a></p><div style="line-height: 1em; margin-bottom: 3em; margin-left: 0.5em; margin-top: -4em; text-align: left;"><big><b>CERTIFICATE</b></big></div><div style="line-height: 1em; margin-bottom: 3em; margin-left: 0.5em; margin-top: -4em; text-align: left;"><big><b><br /></b></big></div><div style="line-height: 1em; margin-bottom: 3em; margin-left: 0.5em; margin-top: -4em; text-align: left;"><big><b><br /></b></big></div></div><div style="padding: 0.5em;"><p style="margin: 0.5em 0px;"><b><span style="color: #202122;">Hello! This is </span><span style="color: red;">official</span><span style="color: #202122;"> Maamedia Tools software.</span></b></p><p style="color: #202122; margin: 0.5em 0px;">Certificate ID is: 78872</p><p style="color: #202122; margin: 0.5em 0px;">See certificates in <a href="https://meta.maamedia.org/wiki/Certified_Maamedia_tools">Maamedia Meta</a>.</p></div></div></div></div></div>
+
